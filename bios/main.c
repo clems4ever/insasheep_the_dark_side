fea7f9aa9a4a5976e2b65611bb0b831f04da3187
Cl3ms_Maul <clems.maul@tatooin.ta>
2013-04-25 17:18:10 +0200

    Get bootloader to work

--- bios/main.c
+++ bios/main.c
@@ -1,4 +1,5 @@
-
+#include "string.h"
+#include "terminal.h"
 #include "uart.h"
 
 #define KERNEL_BASE (void*)0x40001000
@@ -6,33 +7,16 @@
 extern void jump(void *addr);
 
 int main(int argc, char **argv){
-		char c;
+		char cmd[50];
 
 		uart_print("========= INSA BOOTLOADER ========\r\n");
 
-		c = '*';
 		while(1){
-				switch(c){
-						case 'k': // Boot kernel
-								uart_println("Booting kernel...");
-								jump(KERNEL_BASE);
-								break;
-						case 'h': // HELP
-								uart_println("**** HELP ****");
-								uart_println("r\treset");
-								uart_println("k\tboots kernel");
-								break;	
-						case 'r':
-								// Jump to the BIOS
-								jump(0x0);
-								break;
-				}
+			getCommand(cmd, 50);
+			parseCommand(cmd);
 
-				uart_print(">> ");
-				c = uart_getchar();
-				uart_print("\r\n");
 		}
 
 		// RESET, never reached because of while(1)
-		jump(0x00);
+		//jump(0x00);
 }
